cd04b135529999e0f3342414fd3844105137dabe
Tanyawat Vittayapalotai <hamzaabamboo@gmail.com>
2019-03-12 12:57:08 +0700

    :sparkles: landing page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,60 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <div class="main">
+      <h1>Coming Soon...</h1>
+      <h3>at</h3>
+      <h2>
+        <a href="https://hamzaabamboo.github.io">hamzaabamboo.github.io</a>
+      </h2>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
 
-@Component({
-  components: {
-    HelloWorld
-  }
-})
+@Component({})
 export default class Home extends Vue {}
 </script>
+
+<style lang="scss" scoped >
+.home {
+  ::before {
+    background-image: url("../assets/bg/1.jpg");
+    opacity: 0.3;
+    content: "";
+    z-index: -9;
+    top: 0;
+    left: 0;
+    height: 100vh;
+    width: 100vw;
+    display: block;
+    position: absolute;
+    background-size: cover;
+    background-position: center top;
+  }
+}
+.main {
+  h1 {
+    font-size: 6em;
+    margin: 10px;
+  }
+  h2 {
+    font-size: 2em;
+    font-weight: 800;
+  }
+  h3 {
+    font-size: 2em;
+  }
+  a {
+    color: black;
+    text-decoration: grey;
+  }
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+</style>
+
