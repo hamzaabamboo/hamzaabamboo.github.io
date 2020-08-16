9fedcfb1a4d03e266670c8bc6e5a2afd6a9e388e
hamzaabamboo <hamzaabamboo@users.noreply.github.com>
2020-08-16 12:52:32 +0000

    deploy: 475b7464ffa91777d6579ecef61aea9117840138

---
+++ precache-manifest.7560960a2dc890751fcfbc3ac2a3f2e6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc0bcb150dcb37947add",
+    "url": "/css/app.14b89edf.css"
+  },
+  {
+    "revision": "3e23ecd6dc03fed7ee5c",
+    "url": "/css/chunk-vendors.354276a6.css"
+  },
+  {
+    "revision": "dd91d80a942d77dc971451cbc32e744f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2dfe98a231ca12e5650c",
+    "url": "/js/about.a0248803.js"
+  },
+  {
+    "revision": "fc0bcb150dcb37947add",
+    "url": "/js/app.14060307.js"
+  },
+  {
+    "revision": "3e23ecd6dc03fed7ee5c",
+    "url": "/js/chunk-vendors.64c4217e.js"
+  },
+  {
+    "revision": "3d501368b57f63119b27f548446a769d",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b0c914076f3f94229fcdcbaa7cc7d8e0.js"
+  "/precache-manifest.7560960a2dc890751fcfbc3ac2a3f2e6.js"
 );
 
-workbox.core.setCacheNameDetails({prefix: "github.io"});
+workbox.core.setCacheNameDetails({prefix: "gh-pages"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,5 +31,4 @@ workbox.core.setCacheNameDetails({prefix: "github.io"});
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
